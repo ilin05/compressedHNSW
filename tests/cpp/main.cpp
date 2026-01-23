7c1b512c7e8405481aa0ebf2325e7421dd3c17f4
fogman <zhanglin20050530@163.com>
2026-01-23 23:17:03 +0800

    add test bigann 1b

--- tests/cpp/main.cpp
+++ tests/cpp/main.cpp
@@ -1,8 +1,23 @@
 
 
+#include <iostream>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
 void sift_test1B();
+void sift_test1B_double_hnsw();
+void sift_test1B_double_hnswalp();
 int main() {
-    sift_test1B();
+#ifdef _OPENMP
+    omp_set_num_threads(20);
+    std::cout << "OpenMP Threads set to: 20" << std::endl;
+#endif
+
+    // sift_test1B();
+    sift_test1B_double_hnsw();
+    sift_test1B_double_hnswalp();
 
     return 0;
 }
