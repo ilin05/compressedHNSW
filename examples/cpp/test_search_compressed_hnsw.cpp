413f2700a3e73705a636bcc3fd1beed83720af5f
fogman <zhanglin20050530@163.com>
2026-04-04 20:41:02 +0800

    add dexor+

--- examples/cpp/test_search_compressed_hnsw.cpp
+++ examples/cpp/test_search_compressed_hnsw.cpp
@@ -222,7 +222,7 @@ int main(int argc, char** argv) {
     };
 
     vector<string> algorithms = {
-        // "DeXOR",
+        "DeXOR",
         // "Gorilla", 
         // "Elf", 
         // "Camel",
