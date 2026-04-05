16b64377cfede162c3eb5997c0223b89acd78828
fogman <zhanglin20050530@163.com>
2026-04-05 15:06:55 +0800

    deubg

--- examples/cpp/test_search_compressed_hnsw.cpp
+++ examples/cpp/test_search_compressed_hnsw.cpp
@@ -207,12 +207,12 @@ int main(int argc, char** argv) {
 
     std::string base_dir = "../datasets/hdf5files/";
     
-    if(argc > 1) {
-        base_dir = argv[1];
-        if(base_dir.back() != '/' && base_dir.back() != '\\') {
-            base_dir += '/';
-        }
-    }
+    // if(argc > 1) {
+    //     base_dir = argv[1];
+    //     if(base_dir.back() != '/' && base_dir.back() != '\\') {
+    //         base_dir += '/';
+    //     }
+    // }
 
     vector<string> base_datasets = {
         "fashion-mnist-784-euclidean",
