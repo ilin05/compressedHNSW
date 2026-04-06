cbd8b8d47aabff0d05bc0c52196fd98122788718
fogman <zhanglin20050530@163.com>
2026-04-06 13:39:10 +0800

    debug

--- examples/cpp/test_build_compressed_hnsw.cpp
+++ examples/cpp/test_build_compressed_hnsw.cpp
@@ -182,12 +182,12 @@ int main(int argc, char** argv) {
 
     std::string base_dir = "../datasets/hdf5files/";
     
-    if(argc > 1) {
-        base_dir = argv[1];
-        if(base_dir.back() != '/' && base_dir.back() != '\\') {
-            base_dir += '/';
-        }
-    }
+    // if(argc > 1) {
+    //     base_dir = argv[1];
+    //     if(base_dir.back() != '/' && base_dir.back() != '\\') {
+    //         base_dir += '/';
+    //     }
+    // }
 
     vector<string> datasets = {
         "fashion-mnist-784-euclidean_train.fvecs",
