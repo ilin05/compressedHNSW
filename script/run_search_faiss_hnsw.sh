8cfd7c817cfe9e171a01cfde424854425cd012b9
fogman <zhanglin20050530@163.com>
2026-04-10 14:58:38 +0800

    add script for testing faiss hnsw pq/sq

---
+++ script/run_search_faiss_hnsw.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "Testing Faiss Build"
+nohup ../build/test_search_faiss_hnsw_pq_sq > test_search_faiss_hnsw_pq_sq_output.log 2>&1
+
+# ../../build/test_build_faiss_hnsw_pq_sq
+
+# echo "Testing Faiss Search..."
+# ../../build/test_search_faiss_hnsw_pq_sq
