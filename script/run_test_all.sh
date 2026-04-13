ae34b57cbdb6b8eb1ddcb366089692f470c23baa
fogman <zhanglin20050530@163.com>
2026-04-13 07:54:46 +0000

    add test results

--- script/run_test_all.sh
+++ script/run_test_all.sh
@@ -16,8 +16,8 @@ echo "==================================="
 echo "==================================="
 echo "2. Running Search Tests"
 echo "==================================="
-bash run_search_hnsw.sh
-bash run_search_hnswalp_pq.sh
+# bash run_search_hnsw.sh
+# bash run_search_hnswalp_pq.sh
 bash run_search_hnswdexor_pq.sh
 # bash test_search_faiss_hnsw_PQ.sh
 # bash test_search_faiss_hnsw_SQ.sh
