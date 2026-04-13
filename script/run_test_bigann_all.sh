7c9f607767c863d60c765ba77cca3e25f71f805e
fogman <zhanglin20050530@163.com>
2026-04-13 12:16:06 +0800

    modify script

--- script/run_test_bigann_all.sh
+++ script/run_test_bigann_all.sh
@@ -6,12 +6,12 @@ echo "Running all HNSW bigann tests"
 echo "==================================="
 echo "1. Running Build Tests"
 echo "==================================="
-bash run_build_bigann_hnsw.sh
-bash run_build_bigann_hnswalp_pq.sh
-bash run_build_bigann_hnswdexor_pq.sh
-bash run_build_bigann_faiss_hnsw_PQ.sh
-bash run_build_bigann_faiss_hnsw_SQ.sh
-bash test_build_bigann_compressed_hnsw_framework/run_build_bigann_compressed_hnsw_framework.sh
+# bash run_build_bigann_hnsw.sh
+# bash run_build_bigann_hnswalp_pq.sh
+# bash run_build_bigann_hnswdexor_pq.sh
+# bash run_build_bigann_faiss_hnsw_PQ.sh
+# bash run_build_bigann_faiss_hnsw_SQ.sh
+# bash test_build_bigann_compressed_hnsw_framework/run_build_bigann_compressed_hnsw_framework.sh
 
 echo "==================================="
 echo "2. Running Search Tests"
@@ -21,7 +21,7 @@ bash run_search_bigann_hnswalp_pq.sh
 bash run_search_bigann_hnswdexor_pq.sh
 bash test_search_bigann_faiss_hnsw_PQ.sh
 bash test_search_bigann_faiss_hnsw_SQ.sh
-bash test_search_compressed_hnsw_framework/run_search_bigann_compressed_hnsw_framework.sh
+# bash test_search_compressed_hnsw_framework/run_search_bigann_compressed_hnsw_framework.sh
 ../build/test_search_bigann_faiss_hnsw_pq_sq --algorithm HNSWPQ HNSWSQ --pq_m 4 8 ----sq_nbits 4 8 --dataset bigann_1M bigann_10M
 
 echo "==================================="
