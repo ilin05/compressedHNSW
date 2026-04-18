9d5f0b513214a0bc1c88cb7d97e5b03db9983c82
fogman <zhanglin20050530@163.com>
2026-04-18 18:35:29 +0800

    add test_compressed_hnsw_framework script

---
+++ script/run_search_compressed_hnsw_framework.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+set -e
+
+echo "Testing hnswlib compressed_hnsw_framework Search"
+../build/test_search_compressed_hnsw --use_tls 1 \
+    --tls_ratio 0.2 \
+    --dataset fashion-mnist-784-euclidean gist-960-euclidean mnist-784-euclidean sift-128-euclidean \
+    --algorithm DeXOR Gorilla Camel Elf DeXORPlus
+
