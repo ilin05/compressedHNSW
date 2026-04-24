d956c7d864d548d43269382d276520c1e75d335c
fogman <zhanglin20050530@163.com>
2026-04-25 00:04:28 +0800

    modify ablation study bash

--- script/run_search_compressed_hnsw_ablation.sh
+++ script/run_search_compressed_hnsw_ablation.sh
@@ -6,11 +6,13 @@ echo "[Ablation] Search compressed_hnsw_framework indexes"
   --threads 32 \
   --k 1 \
   --dataset fashion-mnist-784-euclidean gist-960-euclidean mnist-784-euclidean sift-128-euclidean \
-  --algorithm DeXOR Gorilla Elf Camel DeXORPlus \
+  # --algorithm DeXOR Gorilla Elf Camel DeXORPlus \
+  --algorithm DeXOR \
   --chain_max 2 5 -1 \
   --use_cache 0 1 \
   --use_tls 0 1 \
   --tls_ratio 0.1 0.2 0.3 0.5 \
-  --output_csv compressed_hnsw_ablation_search_results.csv
+  # --output_csv compressed_hnsw_ablation_search_results.csv
+  --output_csv compressed_hnsw_DeXOR_sift_ablation_search_results.csv
 
 echo "Saved CSV: compressed_hnsw_ablation_search_results.csv"
