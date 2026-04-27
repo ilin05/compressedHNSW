9e7ea8d9c6d087fde93542aa64970867057ca9d9
fogman <zhanglin20050530@163.com>
2026-04-27 02:31:15 +0000

    add test profiling output

--- script/run_search_compressed_hnsw_ablation.sh
+++ script/run_search_compressed_hnsw_ablation.sh
@@ -5,13 +5,13 @@ echo "[Ablation] Search compressed_hnsw_framework indexes"
 ../build/test_search_compressed_hnsw_ablation \
   --threads 1 \
   --k 1 \
-  --dataset sift-128-euclidean \
+  --dataset gist-960-euclidean \
   --algorithm DeXOR \
   --chain_max 2 5 -1 \
   --use_cache 0 1 \
   --use_tls 0 1 \
   --tls_ratio 0.1 0.2 0.3 0.5 \
-  --output_csv compressed_hnsw_DeXOR_sift_ablation_search_results.csv
+  --output_csv compressed_hnsw_DeXOR_gist_ablation_search_results.csv
 
 echo "Saved CSV: compressed_hnsw_ablation_search_results.csv"
 # --dataset fashion-mnist-784-euclidean gist-960-euclidean mnist-784-euclidean sift-128-euclidean \
