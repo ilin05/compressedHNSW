390b0608663fc2811f4555ebf5b7c33f43dd7185
fogman <zhanglin20050530@163.com>
2026-05-02 23:55:16 +0800

    add VQ test

--- script/run_vq_recall.sh
+++ script/run_vq_recall.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+echo "Testing Faiss NSG Build"
+# Build NSG indexes for the four in-memory datasets
+../build/test_build_faiss_nsg \
+	--dataset fashion-mnist-784-euclidean gist-960-euclidean mnist-784-euclidean sift-128-euclidean \
+	--R 32 \
+	--normal_base_dir ../datasets/hdf5files/
+
 echo "Testing VQ-Recall on HNSW / Compressed-HNSW / IVF / NSG"
 # Measure recall and QPS with the unified VQ driver
 ../build/test_vq_recall \
