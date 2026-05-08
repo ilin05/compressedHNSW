8944a7ccef334e699313d936be3126813a87f3e1
fogman <zhanglin20050530@163.com>
2026-05-08 16:14:15 +0800

    modify test build compression hnsw framework ablation

--- script/run_build_compressed_hnsw_ablation.sh
+++ script/run_build_compressed_hnsw_ablation.sh
@@ -4,9 +4,9 @@ set -e
 echo "[Ablation] Build compressed_hnsw_framework indexes"
 ../build/test_build_compressed_hnsw_ablation \
   --threads 32 \
-  --dataset deep-image-96-angular \
+  --dataset fashion-mnist-784-euclidean gist-960-euclidean mnist-784-euclidean sift-128-euclidean \
   --algorithm DeXOR Gorilla Elf Camel \
-  --chain_max -1 \
+  --chain_max 3 4 6 7 8 \
   --output_csv compressed_hnsw_ablation_build_results.csv
 
 echo "Saved CSV: compressed_hnsw_ablation_build_results.csv"
