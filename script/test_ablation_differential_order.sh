df4c68b9e399581a3f62f5705eed5a687d13a328
fogman <zhanglin20050530@163.com>
2026-05-08 13:29:26 +0800

    debug

--- script/test_ablation_differential_order.sh
+++ script/test_ablation_differential_order.sh
@@ -2,4 +2,4 @@
 set -e
 
 echo "Testing Ablation Differential Order"
-../build/test_ablation_differential_order --dataset mnist-784-euclidean sift-128-euclidean --algorithm DeXOR Elf Gorilla Camel
+../build/test_ablation_differential_order --dataset gist-960-euclidean fashion-mnist-784-euclidean --algorithm DeXOR Elf Gorilla Camel
