eb71eb78b346a132aac8c233ddb82868548a0ef0
fogman <zhanglin20050530@163.com>
2026-06-09 17:45:14 +0800

    modify test ablation differential order

--- script/test_ablation_differential_order.sh
+++ script/test_ablation_differential_order.sh
@@ -2,4 +2,4 @@
 set -e
 
 echo "Testing Ablation Differential Order"
-../build/test_ablation_differential_order --dataset gist-960-euclidean fashion-mnist-784-euclidean deep-image-96-angular mnist-784-euclidean sift-128-euclidean --algorithm DeXOR Elf Gorilla Camel
+../build/test_ablation_differential_order --dataset gist-960-euclidean fashion-mnist-784-euclidean mnist-784-euclidean sift-128-euclidean --algorithm DeXOR Elf Gorilla Camel
